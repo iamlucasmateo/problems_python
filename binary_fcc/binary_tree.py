a19b02e3be5f0153139f173ac1861039090567e0
iamlucasmateo <perrone.lucasmateo@gmail.com>
2023-06-16 07:39:01 +0200

    depth first search

--- binary_fcc/binary_tree.py
+++ binary_fcc/binary_tree.py
@@ -4,7 +4,9 @@ class Node:
         self.left = None
         self.right = None
     
-
+class Tree:
+    def __init__(self, root: Node):
+        self.root = root
 
 
 a = Node("a")
@@ -19,4 +21,31 @@ a.right = c
 b.left = d
 b.right = e
 c.right = f
+
+tree = Tree(root=a)
+
+
+#         a
+#        / \
+#       b   c
+#      / \   \
+#     d   e   f
+
+# Depth first: a, b, d, e, c, f
+def get_depth_first_values(tree: Tree):
+    values = []
+    stack = []
+    current_node = tree.root
+    stack.append(current_node)
+    while len(stack) > 0:
+        values.append(current_node.value)
+        if current_node.right is not None:
+            stack.append(current_node.right)
+        if current_node.left is not None:
+            stack.append(current_node.left)
+        current_node = stack.pop()
+    
+    return values
+
+# Breadth first: a, b, c, d, e, f
 
